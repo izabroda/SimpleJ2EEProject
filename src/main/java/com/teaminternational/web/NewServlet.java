b58a19d879207a1968d95d5784fba1abe18ae5a3
izabroda <izabroda@mail.ua>
2017-10-26 16:56:26 +0300

    1-st commit

---
+++ src/main/java/com/teaminternational/web/NewServlet.java
@@ -0,0 +1,17 @@
+package com.teaminternational.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Stud on 26.10.2017.
+ */
+public class NewServlet extends BaseHttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
